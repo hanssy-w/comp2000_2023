f2f5f634eabddb73efdb5c003dab36ba1d0b44a3
hanssy-w <hanswanghanyang545@gmail.com>
2023-08-15 13:51:27 +1000

    initial commit

--- src/Actor.java
+++ src/Actor.java
@@ -1,14 +1,23 @@
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Polygon;
+import java.util.ArrayList;
 
 public abstract class Actor {
   Color color;
   Cell loc;
+  // ArrayList<Polygon> polygons;
+  ArrayList<Polygon> poly = new ArrayList<Polygon>();
 
   public void paint(Graphics g) {
-    g.setColor(color);
-    g.fillRect(loc.x + 5, loc.y + 5, loc.width - 10, loc.height - 10);
-    g.setColor(Color.GRAY);
-    g.drawRect(loc.x + 5, loc.y + 5, loc.width - 10, loc.height - 10);
+    for(Polygon polygon : poly){
+      g.setColor(color);
+      g.drawPolygon(polygon);
+      g.fillPolygon(polygon);
+    // g.setColor(color);
+    // g.fillRect(loc.x + 5, loc.y + 5, loc.width - 10, loc.height - 10);
+    // g.setColor(Color.GRAY);
+    // g.drawRect(loc.x + 5, loc.y + 5, loc.width - 10, loc.height - 10);
+    }
   }
 }
--- src/Stage.java
+++ src/Stage.java
@@ -1,23 +1,31 @@
 import java.awt.Graphics;
+import java.util.List;
 import java.awt.Point;
+import java.util.ArrayList;
 
 public class Stage {
   Grid grid;
-  Actor cat;
-  Actor dog;
-  Actor bird;
+  // Actor cat;
+  // Actor dog;
+  // Actor bird;
+  List<Actor> actors; // = new ArrayList<Actor>();
 
   public Stage() {
     grid = new Grid();
-    cat = new Cat(grid.cellAtColRow(0, 0));
-    dog = new Dog(grid.cellAtColRow(0, 15));
-    bird = new Bird(grid.cellAtColRow(12, 9));
+    actors = new ArrayList<>();
+
+    actors.add(new Cat(grid.cellAtColRow(0, 0)));
+    actors.add(new Dog(grid.cellAtColRow(0, 15)));
+    actors.add(new Bird(grid.cellAtColRow(12, 9)));
   }
 
   public void paint(Graphics g, Point mouseLoc) {
     grid.paint(g, mouseLoc);
-    cat.paint(g);
-    dog.paint(g);
-    bird.paint(g);
+    for(Actor actor:actors){
+    // cat.paint(g);
+    // dog.paint(g);
+    // bird.paint(g);
+    actor.paint(g);
+    }
   }
 }
